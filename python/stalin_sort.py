e0e442a632332f389b0f716143a1a6882e92eb9f
João Eduardo Montandon <edu.montandon@gmail.com>
2018-11-07 11:47:33 -0200

    Fixed issue #12

--- python/stalin_sort.py
+++ python/stalin_sort.py
@@ -1,5 +1,9 @@
 def sort(list):
-    return [v for i, v in enumerate(list) if i == 0 or v >= list[i - 1]]
+    sorted_list = [list[0]]
+    for e in list[1:]:
+        if e >= sorted_list[-1]:
+            sorted_list.append(e)
+    return sorted_list
 
 ordered_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 sorted_list = sort(ordered_list)
@@ -12,4 +16,8 @@ print("Original: {0}\nSorted: {1}\n".format(reversed_list, sorted_list))
 mixed_list = [1, 3, 2, 5, 4, 7, 6, 9, 8]
 sorted_list = sort(mixed_list)
 print("Original: {0}\nSorted: {1}\n".format(mixed_list, sorted_list))
+
+test_list = [1,5,2,4]
+sorted_list = sort(test_list)
+print("Original: {0}\nSorted: {1}\n".format(test_list, sorted_list))
 
