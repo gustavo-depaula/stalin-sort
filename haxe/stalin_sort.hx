35f91b90f2a3e33e3d3635187c4e9a8d1a96331b
Delta114514 <gey3933@gmail.com>
2019-07-30 16:21:24 +0900

    add Haxe implementation

---
+++ haxe/stalin_sort.hx
@@ -0,0 +1,20 @@
+class StalinSort{
+    public static function sort(array:Array<Int>):Array<Int>{
+        var maxValue:Int = array[0];
+        var sortedArray:Array<Int> = [array[0]];
+
+        for (index in 1...array.length){
+            if (array[index] >= maxValue){
+                sortedArray.push(array[index]);
+                maxValue = array[index];
+            }
+        }
+        return sortedArray;
+    }
+}
+
+class Main{
+    public static function main(){
+        trace(StalinSort.sort([1, 2, 3, 5, 4, 5, 2, 1]));
+    }
+}
