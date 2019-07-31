0f7a4624d9351b183683b195e1c4ecd756f299da
Maurício Szabo <mauricio.szabo@gmail.com>
2019-07-31 11:08:23 -0300

    Added "strict" version

--- clojure/stalin_sort.clj
+++ clojure/stalin_sort.clj
@@ -1,7 +1,12 @@
 (ns stalin-sort)
 
-(defn stalin-sort [coll]
+(defn lazy [coll]
   (if (empty? coll)
     []
     (let [[f & rs] coll]
-      (->> rs (filter #(<= f %)) stalin-sort (cons f) lazy-seq))))
+      (->> rs (filter #(<= f %)) lazy (cons f) lazy-seq))))
+
+(defn strict [coll]
+  (if (empty? coll)
+    []
+    (reduce #(cond-> %1 (<= (peek %1) %2) (conj %2)) [(first coll)] (rest coll))))
