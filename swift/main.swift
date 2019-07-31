5e9fd497306d45233e98fe6af15d45a992899bcb
ryoya_ito <horusu.dqx@gmail.com>
2019-08-01 03:46:05 +0900

    add main.swift for executing sample of StalinSort.swift

---
+++ swift/main.swift
@@ -0,0 +1,11 @@
+// To execute this code
+// > swiftc main.swift StalinSort.swift -o main
+// > ./main
+
+let arr = [1, 2, 10, 3, 4, 5, 15, 6, 30, 20]
+let res = arr.stalinSort()
+
+print("Before")
+print(arr)
+print("After")
+print(res)
