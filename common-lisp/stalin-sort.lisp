5095fbf530099c42ee8e092240ca5fddcb91a873
Lucas Vieira <lucasvieira@protonmail.com>
2019-07-31 02:22:43 -0300

    Allow repeated consecutive values in Common Lisp version

--- common-lisp/stalin-sort.lisp
+++ common-lisp/stalin-sort.lisp
@@ -2,7 +2,7 @@
   (let ((first (car list))
 	(rest  (cdr list)))
     (cond ((null rest) list)
-	  ((>= first (car rest))
+	  ((> first (car rest))
 	   (stalin-sort (cons first (cdr rest))))
 	  (t (cons first (stalin-sort rest))))))
 
@@ -15,4 +15,5 @@
 	'((1 2 4 3 6 8 0 9 5 7)
 	  (9 8 7 6 5 4 3 2 1 0)
 	  (1 3 2 5 4 7 0 9 8 6))))
+
 
