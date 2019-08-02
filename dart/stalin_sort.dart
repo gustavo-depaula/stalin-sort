94772570653d172a4a7ec6528af6dbce75cd19e9
akiakishitai <35886966+akiakishitai@users.noreply.github.com>
2019-08-02 22:06:56 +0900

    Update stalin_sort.dart

--- dart/stalin_sort.dart
+++ dart/stalin_sort.dart
@@ -7,8 +7,3 @@ List<T> stalinSort<T extends num>(List<T> list) {
       return (x = value >= x ? value : x) != value;
     });
 }
-
-main() {
-  final list = [1, 2, 4, 3, 8, 0, 5, 9, 7, 11];
-  print(stalinSort(list)); // => [1, 2, 4, 8, 9, 11]
-}
