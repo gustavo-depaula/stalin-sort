fcb5e8e27cf50c9ad3e1d55bcd514a57a59ff483
alphya <merukun1125@gmail.com>
2019-08-05 16:31:26 +0900

    fix bug

--- C++/compitle_time_salin_sort_using_cpp17_and_boost.cpp
+++ C++/compitle_time_salin_sort_using_cpp17_and_boost.cpp
@@ -5,34 +5,43 @@
 
 namespace hana = boost::hana;
 
-template <auto ... args, auto ...i>
+template <auto ... args, auto ... i>
 inline constexpr auto make_tuple_helper(std::index_sequence<i...>) noexcept
 {
     return hana::make_tuple(hana::arg<i>(args...)...);
 }
 
 template <auto itr, auto end, auto ... args, auto ... result_indexes>
-inline constexpr decltype(auto) stalin_impl(std::index_sequence<result_indexes...>) noexcept
-{   
+inline constexpr auto stalin_impl(std::index_sequence<result_indexes...>) noexcept
+{
     if constexpr (sizeof...(args) == 0) return std::index_sequence<>{};
-    
-    else if constexpr (itr == end) return std::index_sequence<result_indexes...>{};
-    
-    else if constexpr (hana::arg<itr>(args...) > hana::arg<itr + 1>(args...))
-        return stalin_impl<itr + 1, end, args...>(std::index_sequence<result_indexes...>{});
-    
-    else return stalin_impl<itr + 1, end, args...>(std::index_sequence<result_indexes..., itr + 1>{});
+
+    else if constexpr (itr == end) 
+        return std::index_sequence<result_indexes...>{};
+
+    else if constexpr (hana::arg<hana::arg<sizeof...(result_indexes)>(result_indexes...)>(args...)
+         > hana::arg<itr + 1>(args...))
+
+        return stalin_impl<itr + 1, end, args...>(
+             std::index_sequence<result_indexes...>{}
+        );
+
+    else return stalin_impl<itr + 1, end, args...>(
+        std::index_sequence<result_indexes..., itr + 1>{}
+    );
 }
 
 template <auto ... args>
 inline constexpr auto stalin_sort() noexcept
 {
-    return make_tuple_helper<args...>(stalin_impl<1, sizeof...(args), args...>(std::index_sequence<1>{}));
+    return make_tuple_helper<args...>(
+        stalin_impl<1, sizeof...(args), args...>(std::index_sequence<1>{})
+    );
 }
 
 auto main() -> int
 {
-    static_assert(stalin_sort<>() == hana::make_tuple());
+    static_assert(stalin_sort<>() == hana::make_tuple()); // コンパイル時にソート完了！
     static_assert(stalin_sort<0>() == hana::make_tuple(0));
     static_assert(stalin_sort<2,3>() == hana::make_tuple(2, 3));
     static_assert(stalin_sort<2,1,3>() == hana::make_tuple(2, 3));
