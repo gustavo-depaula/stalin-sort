80d7cae2fa56a14e566d38a8c4ad247cd95bb802
Bill Wood <wpwoodjr@gmail.com>
2019-08-09 14:37:29 -0400

    pass vector instead of slice

--- rust/in-place-order-N.rs
+++ rust/in-place-order-N.rs
@@ -1,29 +1,31 @@
 // Author Bill Wood
 // in-place, order N
 fn main() {
-    let mut x = vec!(1,2,2,3,2,1,5,8,4,9,);
-    println!("{:?}", stalin_sort(&mut x));
-    let mut x = vec!('a','b','c','b','e','g','d','x');
-    println!("{:?}", stalin_sort(&mut x));
-    let mut ideologies = vec!(
+    let x = vec!(1,2,2,3,2,1,5,8,4,9,);
+    println!("{:?}", stalin_sort(x));
+    let x = vec!('a','b','c','b','e','g','d','x');
+    println!("{:?}", stalin_sort(x));
+    let ideologies = vec!(
         "Fully Automated Luxury Space Communism",
         "Socialism",
         "Capitalism",
         "Communism",
     );
-    println!("{:?}", stalin_sort(&mut ideologies));
+    println!("{:?}", stalin_sort(ideologies));
 }
 
-fn stalin_sort<T: PartialOrd>(v: &mut [T]) -> &[T] {
+fn stalin_sort<T: PartialOrd>(mut v: Vec<T>) -> Vec<T> {
     let mut i = 1;
-    let mut len = v.len();
-    while i < len {
-        v.swap(i, i + v.len() - len);
+    let vlen = v.len();
+    let mut newlen = vlen;
+    while i < newlen {
+        v.swap(i, i + vlen - newlen);
         if v[i] >= v[i - 1] {
             i += 1;
         } else {
-            len -= 1;
+            newlen -= 1;
         }
     }
-    &v[..len]
+    v.truncate(newlen);
+    v
 }
