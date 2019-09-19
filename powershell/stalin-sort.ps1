11214d0a711d456539e6cb07ec54bef70d821673
Dmitry Shaburov <shaburov.d@gmail.com>
2019-09-19 16:35:42 +0300

    implemented stalin-sort in powershell

---
+++ powershell/stalin-sort.ps1
@@ -0,0 +1,23 @@
+function Invoke-StalinSort {
+    [CmdletBinding()]
+    [OutputType([Int32[]])]
+    param(
+        [Parameter(Mandatory=$true)]
+        [Int32[]]
+        $Array
+    )
+
+    $high = $null
+    $result = @()
+
+    foreach ($item in $Array) {
+        if (($null -eq $high) -or ($item -gt $high)) {
+            $high = $item
+            $result += $item
+        }
+    }
+
+    $result
+}
+
+(Invoke-StalinSort @(1, 2, 4, 3, 6, 8, 0, 8, 9, 5, 7)) -join ','
