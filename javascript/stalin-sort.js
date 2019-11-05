b42219e57c3d2ec99280c549e58e1fb86d41dac7
Victor Perin <victor.perin@lambda3.com.br>
2019-11-05 17:52:26 -0300

    refactor(javascript): using @dimabory suggestion

--- javascript/stalin-sort.js
+++ javascript/stalin-sort.js
@@ -1,11 +1,12 @@
 function stalinSort(array) {
   if (!Array.isArray(array)) throw new TypeError('Argument must be an Array!');
 
-  return array.reduce((prev, next) => {
-    if(prev.length === 0) return [next]
-    if(next >= prev[prev.length - 1]) return [...prev, next]
-    return prev
-  }, []);
+  return array.reduce((prev, next) =>
+    !prev.length ||
+      next >= prev[prev.length - 1] ?
+        [...prev, next] :
+        prev
+  , []);
 }
 
 module.exports = stalinSort
