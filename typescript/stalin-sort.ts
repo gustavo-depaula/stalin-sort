030d933647e41c08421e9bd00da16d913a423c04
Victor Perin <victor.perin@lambda3.com.br>
2019-11-05 17:55:54 -0300

    refactor(typescript): using @dimabory suggestion

--- typescript/stalin-sort.ts
+++ typescript/stalin-sort.ts
@@ -1,7 +1,8 @@
 export function stalinSort(arr: number[]): number[] {
-  return arr.reduce((prev: number[], next: number) => {
-    if(prev.length === 0) return [next]
-    if(next >= prev[prev.length - 1]) return [...prev, next]
-    return prev
-  }, []);
+  return arr.reduce<number[]>((prev, next) =>
+    !prev.length ||
+      next >= prev[prev.length - 1] ?
+      [...prev, next] :
+      prev
+  , []);
 }
