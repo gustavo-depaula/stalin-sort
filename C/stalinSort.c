70fd5a2a7daae1af72a56a163df01c9edf97b335
Samuel Hunter <samuelhunter1024@gmail.com>
2020-05-14 14:12:27 -0700

    C: remove malloc by making sortedArr static
    Malloc can cause a bit of headache -- if the OS says they're out of
    memory, then instead of a normal exception-and-stack-trace that
    langauges like Java provide, C would return NULL and acting on the array
    will crash with the cryptic "Segmentation fault" error. Not to mention,
    realloc()ing the array on every item insert can give the OS a CPU-biting
    headache at large volumes.
    
    Instead, statically allocating sortedArr[10] means that stalinSort()
    does not have to worry about memory allocation. Ostensibly, this *is* a
    small enough program that reallocating the array in total times and
    allocating only 10 integers really shouldn't be a problem, but this
    refactoring does give a better idea at what idiomatic C could look like.

--- C/stalinSort.c
+++ C/stalinSort.c
@@ -1,35 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int *stalinSort(int *arr, int size, int *newSize) {
+int stalinSort(int *arr, int size, int *sortedArr) {
+    int newSize = 1;
     int i;
-    int *sortedArr;
 
-    sortedArr = (int*) malloc(sizeof(int));
     sortedArr[0] = arr[0];
-
     for (i = 1; i < size; ++i) {
-        if (arr[i] >= sortedArr[(*newSize)-1]) {
-            ++(*(newSize));
-            sortedArr = (int*) realloc(sortedArr, *newSize * sizeof(int));
-            sortedArr[*newSize-1] = arr[i];
+        if (arr[i] >= sortedArr[newSize-1]) {
+            ++newSize;
+            sortedArr[newSize-1] = arr[i];
         }
     }
-    return sortedArr;
+
+    return newSize;
 }
 
 int main(int argc, char const *argv[]) {
-    int i, newSize = 1;
-    int *sortedArr;
+    int i, newSize;
+    int sortedArr[10];
     int array[10] = {1, 2, 4, 3, 6, 8, 0, 9, 5, 7};
-    
-    sortedArr = stalinSort(array, 10, &newSize);
+
+    newSize = stalinSort(array, 10, sortedArr);
 
     for (i = 0; i < newSize; ++i)
         (void) printf("%i ", sortedArr[i]);
 
     (void) printf("\n");
-
-    free(sortedArr);
     return 0;
 }
