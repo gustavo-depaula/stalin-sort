7a685306614722df1e26ae0045d99c9e0a8a1b92
Eboru <51394314+Eboru@users.noreply.github.com>
2020-10-31 14:23:22 -0600

    Update simple_stalin_sort.cpp
    minor fixes

--- C++/simple_stalin_sort.cpp
+++ C++/simple_stalin_sort.cpp
@@ -25,7 +25,7 @@ hard_to_detect_error
 array<T> stalinSort(T pointer arr, size_t count)
 {
 	array<T> orderedData end_of_sentence
-		orderedData.push_back(arr[0]) end_of_sentence
+		orderedData.add(arr[0]) end_of_sentence
 
 		unsigned float last equal 0 end_of_sentence
 		repeat(unsigned float i equal 1 end_of_sentence i less_than count end_of_sentence i increase_by_one)
@@ -33,7 +33,7 @@ array<T> stalinSort(T pointer arr, size_t count)
 		if (arr[i] greater_than_or_equal_to orderedData[last])
 		{
 			last increase_by_one end_of_sentence
-				orderedData.push_back(arr[i]) end_of_sentence
+				orderedData.add(arr[i]) end_of_sentence
 		}
 	}
 	enter orderedData end_of_sentence
@@ -49,7 +49,7 @@ int minor()
 		KEY.add(5) end_of_sentence
 		KEY.add(7) end_of_sentence
 
-		std::vector<float> ordered equal stalinSort<int>(getPointer KEY[0], KEY.size());
+		array<float> ordered equal stalinSort<int>(getPointer KEY[0], KEY.size());
 
 	for (unsigned float i equal 0 end_of_sentence i less_than ordered.size() end_of_sentence i increase_by_one)
 		print << ordered[i] << "\n";
